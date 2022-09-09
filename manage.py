32b26bbbdffa16f6848bef760fecb1dff1787fc1
xuhonglu <1980098029@qq.com>
2022-09-09 11:05:35 +0800

    master tijiao

--- manage.py
+++ manage.py
@@ -20,8 +20,8 @@ def main():
 
 if __name__ == '__main__':
     main()
-    print(1)
-    print(2)
-    print(3)
-    print(4)
-    print(5)
+    print(6)
+    print(6)
+    print(6)
+    print(6)
+    print(6)
